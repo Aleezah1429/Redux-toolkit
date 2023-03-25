168e35dc6740d4ceb5829740b1ae3707cc31ddde
Aleezah Ikram <64039135+Aleezah1429@users.noreply.github.com>
2023-03-26 02:22:11 +0500

    Add files via upload

--- src/store/slices/UserSlice.jsx
+++ src/store/slices/UserSlice.jsx
@@ -3,6 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
   name: "user",
   initialState: [],
+
+  // one time function use (for e.g. Only in admin) --> use reducers
   reducers: {
     addUser(state, action) {
       state.push(action.payload);
@@ -13,11 +15,18 @@ const userSlice = createSlice({
       console.log("delete user id in user slice => ", action.payload);
     },
     clearAllUsers(state, action) {
-      // return (state = []); //not correct way
-      return []; //initial state empty
-      // console.log("delete all users id in user slice => ", action.payload);
+      // return (state = []); //not correct way, assign empty array
+      return []; //initial state empty, not assign
     },
   },
+
+  // // not for userSlice, suppose for admin or teacher slice
+  // // more than one time function use (for e.g. Use in admin, teacher) --> use extraReducers
+  // extraReducers(builder) {
+  //   builder.addCase(userSlice.actions.clearAllUsers, () => {
+  //     return [];
+  //   }); //addCase is by default function which allow us to use same function
+  // },
 });
 
 console.log("userSlice", userSlice.actions);
